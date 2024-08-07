182871c4d99788fba868bf972c77484ceaa0a0e7
Rodrigo Monteiro Junior <71048858+TsukiGva2@users.noreply.github.com>
2024-08-07 09:25:14 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 'On');
 
 define('SITE_NAME', 'portifolio');
 
-define("SITE", "https://" . $_SERVER['SERVER_NAME'] . "/" . SITE_NAME);
+define("SITE", "https://" . $_SERVER['SERVER_NAME'] . "/";
 
 date_default_timezone_set('America/Sao_Paulo');
 
